294497cdba583c4ad31977f4e76b47ba7121567e
notoaki15 <siderop0ne0@gmail.com>
2016-05-09 17:00:26 +0900

    Create ClassPen.java

---
+++ ClassPen.java
@@ -0,0 +1,20 @@
+class pen{
+	void show(){
+		System.out.println("これはボールペンです");
+	}
+}
+
+class pen2 extends pen {
+	void show(){
+		System.out.println("これは３色ボールペンです");
+	}
+}/////class pen を継承
+
+class main{
+	public static void main(String[]args){
+		pen2 pen2 = new pen2();///class pen を継承した pen2 のインスタンスを生成
+		
+		pen2.show();
+		pen2.show();
+	}
+	}
